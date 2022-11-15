a55e5d962b42c80e7cb097f0ed532b77d2163db3
dragonflypie <paper-tiger@yandex.ru>
2022-11-15 14:56:29 +0400

    feat: implement routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,20 @@
 import { ColorModeContext, useMode } from "./theme";
 import { CssBaseline, ThemeProvider } from "@mui/material";
+import { Route, Routes } from "react-router-dom";
+import Topbar from "./scenes/global/Topbar";
+import Dashboard from "./scenes/dashboard";
+import Sidebar from "./scenes/global/Sidebar";
+
+// import Team from "./scenes/team/Team";
+// import Faq from "./scenes/faq/Faq";
+// import Pie from "./scenes/pie/Pie";
+// import Line from "./scenes/line/Line";
+// import Invoices from "./scenes/invoices/Invoices";
+// import Contacts from "./scenes/contacts/Contacts";
+// import Geography from "./scenes/geography/Geography";
+// import Form from "./scenes/form/Form";
+// import Bar from "./scenes/bar/Bar";
+// import Calendar from "./scenes/calendar/Calendar";
 
 function App() {
   const { theme, colorMode } = useMode();
@@ -9,7 +24,23 @@ function App() {
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <div className="app">
-          <main className="content"></main>
+          <Sidebar />
+          <main className="content">
+            <Topbar />
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+              {/* <Route path="/team" element={<Team />} />
+              <Route path="/faq" element={<Faq />} />
+              <Route path="/pie" element={<Pie />} />
+              <Route path="/line" element={<Line />} />
+              <Route path="/invoices" element={<Invoices />} />
+              <Route path="/contacts" element={<Contacts />} />
+              <Route path="/geography" element={<Geography />} />
+              <Route path="/bar" element={<Bar />} />
+              <Route path="/form" element={<Form />} />
+              <Route path="/calendar" element={<Calendar />} /> */}
+            </Routes>
+          </main>
         </div>
       </ThemeProvider>
     </ColorModeContext.Provider>
---
+++ src/scenes/global/Topbar.tsx
@@ -0,0 +1,54 @@
+import { Box, useTheme, IconButton, useColorScheme, Icon } from "@mui/material";
+import { useContext } from "react";
+import InputBase from "@mui/material/InputBase";
+import { ColorModeContext, tokens } from "../../theme";
+import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined";
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined";
+import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
+import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
+import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
+
+const Topbar = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  const colorMode = useContext(ColorModeContext);
+
+  return (
+    <Box display="flex" justifyContent="space-between" p={2}>
+      <Box
+        sx={{
+          display: "flex",
+          backgroundColor: colors.primary[400],
+          borderRadius: "3px",
+        }}
+      >
+        <InputBase sx={{ ml: 2, flex: 1 }} placeholder="Search"></InputBase>
+        <IconButton type="button" sx={{ p: 1 }}>
+          <SearchOutlinedIcon />
+        </IconButton>
+      </Box>
+
+      <Box sx={{ display: "flex" }}>
+        <IconButton onClick={colorMode.toggleColorMode}>
+          {theme.palette.mode === "light" ? (
+            <DarkModeOutlinedIcon />
+          ) : (
+            <LightModeOutlinedIcon />
+          )}
+        </IconButton>
+        <IconButton>
+          <NotificationsOutlinedIcon />
+        </IconButton>
+        <IconButton>
+          <SettingsOutlinedIcon />
+        </IconButton>
+        <IconButton>
+          <PersonOutlinedIcon />
+        </IconButton>
+      </Box>
+    </Box>
+  );
+};
+
+export default Topbar;
